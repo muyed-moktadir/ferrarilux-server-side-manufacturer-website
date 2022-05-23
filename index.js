15da163a91dc00574ab0b19ca3c6d1af35aa769a
muyed moktadir <muyed004@gmail.com>
2022-05-23 16:52:05 +0600

    db connect

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
-require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -10,11 +10,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7epw5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db("doctors_portal").collection("parts");
+
+
+    // TODO:get all part 
+
+
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
-    res.send("Hello from ferrarilux Portal");
-  });
-  
-  app.listen(port, () => {
-    console.log(`listening on ferrarilux Portal port: ${port}`);
-  });
+  res.send("Hello from ferrarilux Portal");
+});
+
+app.listen(port, () => {
+  console.log(`listening on ferrarilux Portal port: ${port}`);
+});
